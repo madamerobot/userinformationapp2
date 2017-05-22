3e11a323edab702363aab714a278c25b5a865514
Valerie Fuchs <hi@valerie-fuchs.de>
2017-05-22 11:09:08 +0200

    Saving copy before restructuring code

--- src/search.js
+++ src/search.js
@@ -1,48 +1,36 @@
-var searchresult = [];
-
 $(document).ready(function() {
     //Listening to key up event
     $("#searchfield").keyup(function () {
-    //By assigning 'this' to variable 'that', all code below
-    //will refer to the searchfield object, even when written
-    //within a nested function and thus having a different scope
-    var that = this,
+ 
     //#searchfield input gets assigned to variable 'value'
     value = $(this).val();
-    console.log("I'm starting the AJAX request now.")
 
+    console.log("I'm starting the AJAX request now.")
+	
+	// //Date.now logic - FILL IN 
+	// setTimeout(ajaxrequest, 3000);
+	
 	$.ajax({
 		type: "POST",
 		url: "/search",
 		data: {"input": value},
 		dataType: "text",
 		success: function(response){	
-			
-			//This is what the response looks like.
-			//It is an object with three properties:
-			//[{"First name: ":"Valerie",
-			//"Last name: ":"Fuchs",
-			//"Email: ":"hi@hi.com"}]
-			
-			console.log("This is the server's response to the AJAX request: "+response);
-			$('#results').text(response);
-
-			var setTimer = function() {
 				
-			var ajaxrequesttime = Date.now();
-			console.log("I'm done with the AJAX request at: "+ajaxrequesttime);
-
-			for (var i=ajaxrequesttime; i < (ajaxrequesttime+3000); i++) {
-			console.log("I'm chilling for 3000ms");
-			}
-	}//closing setTimer
-
-	setTimer();
-		
+		var responseobject = JSON.parse(response);
+		var input = $("#searchfield");
+	
+		for (var i=0; i < responseobject.length; i++){
+			console.log("This is the server's response to the AJAX request: "+responseobject[i]);
+			var searchresult = responseobject[i].firstname+" "+responseobject[i].lastname
+			$('#searchfield').append(searchresult);
+		}
+	
 		}//closing success function
-	 });//closing ajax
-	
-	
-
+	});//closing ajax
 	});//cosing searchfield keyup
+
 });//cosing document ready
+
+
+
